06bf12ec6e1d00614185ac6c83c9781f8378cba8
kunal <kunalsuraniya123@gmail.com>
2025-11-20 05:57:37 +0530

    modified some functionality

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -6,19 +6,27 @@ const verifyToken = (req, res, next) => {
   const token = req.cookies.token; // Access the cookie using cookie-parser
 
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    return res.status(401).json({ message: 'Unauthorized: No token provided' })
   }
 
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(403).json({ message: 'Forbidden: Invalid token' });
+      return res.status(403).json({ message: 'Forbidden: Invalid token' })
     }
     req.user = decoded; // Add user payload to request object
-    next();
-  });
-};
+    next()
+  })
+}
 
 app.get('/profile', verifyToken, (req, res) => {
   res.status(200).json({ message: `Welcome ${req.user.email}! This is a protected route.` });
-});
-
+})
+app.get('/chat', verifyToken, (req, res) => {
+  res.status(200).json({ message: `Welcome ${req.user.email}! This is a protected route.` });
+})
+app.get('/post', verifyToken, (req, res) => {
+  res.status(200).json({ message: `Welcome ${req.user.email}! This is a protected route.` });
+})
+app.get('/room', verifyToken, (req, res) => {
+  res.status(200).json({ message: `Welcome ${req.user.email}! This is a protected route.` });
+})
