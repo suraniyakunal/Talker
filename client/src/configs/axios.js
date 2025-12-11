7cd9aa6809d81ba267add1b8e92b9b37c8b1dc16
kunal <kunalsuraniya123@gmail.com>
2025-12-12 00:42:18 +0530

    corrected the paths

--- client/src/configs/axios.js
+++ client/src/configs/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "https://talker-bvax.onrender.com", // your backend base URL
+  baseURL: "https://talker-bvax.onrender.com/api", // your backend base URL
   withCredentials: true,                // send cookies with requests
   timeout: 10000, // optional: request timeout in ms
 })
