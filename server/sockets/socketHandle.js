04e3b1530d2fe140796fd1baa0e50c37bacc24ab
kunal <kunalsuraniya123@gmail.com>
2025-12-15 01:33:18 +0530

    backend socketconnection

--- server/sockets/socketHandle.js
+++ server/sockets/socketHandle.js
@@ -63,11 +63,11 @@ const initializeSocketConnection = async (server) => {
     })
 
 
-    socket.on('joinRoom', async (data) => {
-      const { roomId, userId } = data
+    socket.on('joinRoom', async ({ roomId }) => {
       socket.join(roomId);
       const getRoom = await Room.findOne({ _id: roomId });
 
+      console.log(getRoom)
       const isOwner = getRoom.owner.toString() === socket.userId;
       const isSpeaker = getRoom.speakers.some(id => id.toString() === socket.userId);
       const role = isOwner || isSpeaker ? 'speaker' : 'listener';
