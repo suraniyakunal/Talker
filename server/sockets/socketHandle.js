8bbe4e8efe65e7702809a6ca3050cc6dc735adc9
kunal <kunalsuraniya123@gmail.com>
2026-01-19 07:28:12 +0530

    implimented the createRoom

--- server/sockets/socketHandle.js
+++ server/sockets/socketHandle.js
@@ -49,8 +49,8 @@ const initializeSocketConnection = async (server) => {
 
     socket.on('disconnect', () => {
       console.log('🧹 Full cleanup for:', socket.id);
-    });
-  });
-};
+    })
+  })
+}
 
 export { initializeSocketConnection };
